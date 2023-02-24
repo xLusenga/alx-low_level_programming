db4020213f692a8d943fbae40e9ba5ea6f9de07d
xLusenga <xlusenga@gmail.com>
2023-02-24 13:35:54 -0800

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * positive_or_negavtive - checks if given number is +, - or 0
+ * positive_or_negative - checks if given number is +, - or 0
  * @i: number to be checked
  * Return: nothing on success
  */
+
 void positive_or_negative(int i)
 {
 	if (i > 0)
